7357c63691e6ce499037c02d924f016a4f05012a
flazarev <lfawe1414333@gmail.com>
2024-04-29 11:34:07 +0300

    map for delivery

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/map')
+def index():
+    return render_template('map.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
