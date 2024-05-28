7100bc44aced5fb010e9224f32a558cb9be79b09
Vladimir <143862659+fastixr@users.noreply.github.com>
2024-05-28 08:33:26 +0300

    update map in index.html

--- static/js/script.js
+++ static/js/script.js
@@ -191,6 +191,23 @@ function init() {
     myMap.geoObjects.add(myPlacemark);
 }
 
+ymaps.ready(init2);
+
+function init2() {
+    var myMap = new ymaps.Map("map2", {
+        center: [55.81020756890799, 37.49832099999996], // Координаты МАИ
+        zoom: 16, // Уровень масштабирования карты
+        controls: ['zoomControl', 'geolocationControl', 'fullscreenControl']
+    });
+
+    var myPlacemark = new ymaps.Placemark([55.81020756890799, 37.49832099999996], {
+        balloonContent: 'Пицца от пашки'
+    });
+
+    myMap.geoObjects.add(myPlacemark);
+}
+
+
 const slides = document.querySelectorAll('.slide');
 let currentImageIndex = 0;
 let slideInterval;
